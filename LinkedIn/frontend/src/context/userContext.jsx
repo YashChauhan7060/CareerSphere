1ba93e2e3abc854312154604af9b9a0edece4ce7
YashChauhan7060 <yc230310@gmail.com>
2026-01-20 01:23:23 +0530

    Uncomment socket.io client initialization

--- LinkedIn/frontend/src/context/userContext.jsx
+++ LinkedIn/frontend/src/context/userContext.jsx
@@ -3,10 +3,9 @@ import { authDataContext } from './AuthContext'
 import axios from 'axios'
 import { useNavigate } from 'react-router-dom'
 export const userDataContext=createContext()
-// import {io} from "socket.io-client"
+import {io} from "socket.io-client"
 
-
-// export let socket=io("https://careersphere-backend3.onrender.com")
+export let socket=io("https://careersphere-backend3.onrender.com")
 function UserContext({children}) {
 let [userData,setUserData]=useState(null)
 let {serverUrl}=useContext(authDataContext)
