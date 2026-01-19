54d43caba411b8e26e1b4b40a3d720c727ce20d8
YashChauhan7060 <yc230310@gmail.com>
2026-01-20 02:08:54 +0530

    Update cookie settings for token in auth controller

--- LinkedIn/backend/controllers/auth.controllers.js
+++ LinkedIn/backend/controllers/auth.controllers.js
@@ -28,12 +28,12 @@ export const signUp=async (req,res)=>{
        })
 
        let token=await genToken(user._id)
-       res.cookie("token",token,{
-        httpOnly:true,
-        maxAge:7*24*60*60*1000,
-        sameSite:"strict",
-        secure:process.env.NODE_ENVIRONMENT==="production"
-       })
+           res.cookie('token', token, {
+          httpOnly: true,
+          secure: true, // Required for HTTPS
+          sameSite: 'none', // Required for cross-origin
+          maxAge: 7 * 24 * 60 * 60 * 1000
+    });
       return res.status(201).json(user)
 
     } catch (error) {
